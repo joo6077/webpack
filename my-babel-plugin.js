4f53c4f501f322a723fc3d9efc83f008e20f9f14
weedskimhj <kimhj@weedscomm.com>
2021-04-21 17:54:33 +0900

    block scoping 바벨 변환

--- my-babel-plugin.js
+++ my-babel-plugin.js
@@ -1,17 +1,13 @@
 module.exports = function myBabelPlugin() {
     return {
         visitor: {
-            Identifier(path) {
-                const name = path.node.name;
+            VariableDeclaration(path) {
+                console.log('VariableDeclaration() kind:', path.node.kind);
 
-                // 바벨이 만든 AST 노드를 출력한다.
-                console.log('Identifier() name:', name)
-
-                // 변환작업: 코드 문자열을 역순으로 변환한다.
-                path.node.name = name
-                    .split('')
-                    .reverse()
-                    .join('')
+                // const => var 변환
+                if (path.node.kind === 'const') {
+                    path.node.kind = 'var'
+                }
             }
         }
     }
