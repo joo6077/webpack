f424f8cafe5af90dcc5c50287ef829da399beb23
weedskimhj <kimhj@weedscomm.com>
2021-04-21 18:00:06 +0900

    바벨 플러그인 적용

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+// babel config 적용
+
+module.exports = {
+    plugins: [
+        '@babel/plugin-transform-strict-mode',
+        '@babel/plugin-transform-arrow-functions',
+        '@babel/plugin-transform-block-scoping',
+    ]
+}
