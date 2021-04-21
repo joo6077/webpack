5aa7c4d0f15ac38256d87d629a3c7efaa91bfb74
weedskimhj <kimhj@weedscomm.com>
2021-04-21 10:04:29 +0900

    css -> js -> css.om 으로 변형해서 돔에 반영시킴.

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [
+                    'style-loader',
                     'css-loader'
                 ]
             }
